6b6b6e1c77b97c4346201624fe2f7482328a2f31
CasperDevs <casperdevs@outlook.es>
2023-02-17 23:36:14 -0600

    Initial commit CASPERDEVS

---
+++ src/CursoPratica/mein.java
@@ -0,0 +1,17 @@
+package CursoPratica;
+
+public class mein {
+    public static void main(String[] args) {
+        System.out.println("Hola desde el helicoptero");
+
+        saludar();
+
+    }
+
+    public static void saludar(){
+
+        System.out.println("Hola desde el metodo saludar chicos, hayyy chicos, me van a romper las ventanadsss");
+    }
+
+
+}
