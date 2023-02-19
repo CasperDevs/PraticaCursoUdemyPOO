6959fc9880d6241c45e037e93ef6f00be4210d91
CasperDevs <casperdevs@outlook.es>
2023-02-18 22:37:51 -0600

    Inicio proyecto curso java orientada a objetos udemy

--- src/CursoPratica/mein.java
+++ src/CursoPratica/mein.java
@@ -1,17 +1,10 @@
 package CursoPratica;
 
 public class mein {
+
     public static void main(String[] args) {
-        System.out.println("Hola desde el helicoptero");
+        System.out.println("Welome to Dayette System");
 
-        saludar();
 
     }
-
-    public static void saludar(){
-
-        System.out.println("Hola desde el metodo saludar chicos, hayyy chicos, me van a romper las ventanadsss");
-    }
-
-
 }
---
+++ src/ProyectoCurso/Min.java
@@ -0,0 +1,8 @@
+package ProyectoCurso;
+
+public class Min {
+    //Entry Point
+    public static void main(String[] args) {
+        System.out.println("Hola Mondo");
+    }
+}
